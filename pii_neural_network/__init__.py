f6df1bb19aee9c489123159cc517fd1d8e978da8
Émilie ROGER <emilie.roger@ensc.fr>
2017-04-20 17:28:27 +0200

    Fichiers pour l’installation (=> bibliothèque)

---
+++ pii_neural_network/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pii_neural_network.couche import *
+from pii_neural_network.fonction import *
+from pii_neural_network.donnees import recuperation
+from pii_neural_network.reseau import Reseau
+
+__all__ = ['couche', 'fonction', 'donnees', 'reseau']
---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+import pii_neural_network
+
+setup(
+    name = 'pii-neural-network',
+    version = '0.0.1',
+    packages = find_packages(),
+    author = 'Émilie ROGER',
+    author_email = 'emilie.roger@ensc.fr',
+    description = 'Permet de créer des réseaux de neurones',
+    long_description = open('README.md').read(),
+    include_package_data = True,
+    url = 'https://github.com/remilieam/pii-neural-network',
+    classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved",
+        "Natural Language :: French",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Communications",
+    ],
+    license = "WTFPL",
+)
